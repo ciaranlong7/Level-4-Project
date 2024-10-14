f5caffdcf4a5950997649896784eff186ab59b77
ciaranlong7 <ciaranlong7@gmail.com>
2024-10-14 17:42:53 +0100

    Starting to plot

--- Test.py
+++ Test.py
@@ -1,5 +1,17 @@
-#Test
-def my_fn(x):
-    return x^2+5
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
 
-my_fn(5)
+with fits.open("spec-8521-58175-0279.fits") as hdul:
+    subset = hdul[1]        
+
+    sdss_flux = subset.data['flux'] # 10-17 ergs/s/cm2/Å
+    sdss_measured_wl = 10**subset.data['loglam'] #Wavelength in Angstroms
+    sdss_flux_unc = np.array([np.sqrt(1/val) if val!=0 else np.nan for val in subset.data['ivar']])
+
+#Plotting sdss flux against wavelength
+plt.plot(sdss_measured_wl, sdss_flux, label = 'Spectrum')
+plt.xlabel('Wavelength / Å', fontsize = 16)
+plt.ylabel('Flux', fontsize = 16)
+plt.legend(loc = 'upper right')
+plt.show()
